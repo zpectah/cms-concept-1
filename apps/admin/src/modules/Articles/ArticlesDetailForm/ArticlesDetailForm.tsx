eea8bfce88ff0bba4d5a72da9923e1785397b9d9
zpecter <sychrat@gmail.com>
2025-11-29 17:21:56 +0100

    articles comments, backup

--- apps/admin/src/modules/Articles/ArticlesDetailForm/ArticlesDetailForm.tsx
+++ apps/admin/src/modules/Articles/ArticlesDetailForm/ArticlesDetailForm.tsx
@@ -18,10 +18,10 @@ import {
   TextareaField,
   WysiwygField,
   AddressField,
-  DebugFormModel,
   GpsPickerField,
   FormContent,
   SwitchField,
+  DynamicPortal,
 } from '../../../components';
 import { useUserActions } from '../../../hooks';
 import { TagsPickerField } from '../../Tags';
@@ -38,6 +38,8 @@ const ArticlesDetailForm = () => {
   const { detailId, form, locale, locales, onSubmit, onLocaleChange, fieldOptions, detailData } =
     useArticlesDetailForm();
 
+  const dynamicSlotId = `portal-transport-articles-${detailId}-comments`;
+
   const type = useWatch({ name: registeredFormFields.type, control: form.control });
   const startDate = useWatch({ name: registeredFormFields.event_start, control: form.control });
   const created = useWatch({ name: registeredFormFields.created, control: form.control });
@@ -46,94 +48,101 @@ const ArticlesDetailForm = () => {
   const isComments = detailId !== newItemKey;
 
   return (
-    <ControlledForm key={detailId} form={form} formProps={{ onSubmit }}>
-      <FormLayout
-        actions={
-          <FormDetailActions detailId={detailId} listPath={`/${routes.articles.path}`} modelActions={modelActions} />
-        }
-        sidebar={
-          <FormDetailSidebar detailId={detailId} created={created} updated={updated}>
-            <SwitchField
-              name={registeredFormFields.approved}
-              fieldProps={{ label: t('form:label.approved') }}
-              isDisabled={detailData?.approved || !modelActions.approve}
-            />
-          </FormDetailSidebar>
-        }
-        actionbar={
-          isComments && <CommentsManager isEnabled={isComments} contentType={modelKeys.articles} contentId={detailId} />
-        }
-      >
-        <InputField name={registeredFormFields.name} label={t('form:label.name')} isRequired />
-        <SelectField
-          name={registeredFormFields.type}
-          label={t('form:label.type')}
-          items={fieldOptions.type}
-          fieldProps={{ defaultValue: articlesTypeDefault, sx: { width: { xs: '100%', md: '33%' } } }}
-        />
-        <HiddenCard visible={type === 'event'} sx={{ marginTop: 1.5 }}>
-          <FormContent>
-            <Grid container spacing={2}>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <DateTimePickerField
-                  name={registeredFormFields.event_start}
-                  label={t('form:label.eventStart')}
+    <>
+      <ControlledForm key={detailId} form={form} formProps={{ onSubmit }}>
+        <FormLayout
+          actions={
+            <FormDetailActions detailId={detailId} listPath={`/${routes.articles.path}`} modelActions={modelActions} />
+          }
+          sidebar={
+            <FormDetailSidebar detailId={detailId} created={created} updated={updated}>
+              <SwitchField
+                name={registeredFormFields.approved}
+                fieldProps={{ label: t('form:label.approved') }}
+                isDisabled={detailData?.approved || !modelActions.approve}
+              />
+            </FormDetailSidebar>
+          }
+          actionbar={<div id={dynamicSlotId} style={{ marginTop: '1rem' }} />}
+        >
+          <InputField name={registeredFormFields.name} label={t('form:label.name')} isRequired />
+          <SelectField
+            name={registeredFormFields.type}
+            label={t('form:label.type')}
+            items={fieldOptions.type}
+            fieldProps={{ defaultValue: articlesTypeDefault, sx: { width: { xs: '100%', md: '33%' } } }}
+          />
+          <HiddenCard visible={type === 'event'} sx={{ marginTop: 1.5 }}>
+            <FormContent>
+              <Grid container spacing={2}>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <DateTimePickerField
+                    name={registeredFormFields.event_start}
+                    label={t('form:label.eventStart')}
+                    isRequired
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <DateTimePickerField
+                    name={registeredFormFields.event_end}
+                    label={t('form:label.eventEnd')}
+                    fieldProps={{ minDate: getTypedDate(startDate) }}
+                    isRequired
+                  />
+                </Grid>
+              </Grid>
+              <AddressField fieldPrefix={registeredFormFields.event_address} disableCard />
+              <GpsPickerField name={registeredFormFields.event_location} label={t('form:label.eventLocation')} />
+            </FormContent>
+          </HiddenCard>
+          <LocalesTabs
+            locales={locales}
+            locale={locale}
+            onLocaleChange={onLocaleChange}
+            render={(loc) => (
+              <>
+                <InputField
+                  name={`${registeredFormFields.locale}.${loc}.${registeredFormFields.title}`}
+                  label={t('form:label.title')}
                   isRequired
                 />
-              </Grid>
-              <Grid size={{ xs: 12, sm: 6 }}>
-                <DateTimePickerField
-                  name={registeredFormFields.event_end}
-                  label={t('form:label.eventEnd')}
-                  fieldProps={{ minDate: getTypedDate(startDate) }}
+                <TextareaField
+                  name={`${registeredFormFields.locale}.${loc}.${registeredFormFields.description}`}
+                  label={t('form:label.description')}
+                />
+                <WysiwygField
+                  name={`${registeredFormFields.locale}.${loc}.${registeredFormFields.content}`}
+                  label={t('form:label.content')}
                   isRequired
                 />
-              </Grid>
-            </Grid>
-            <AddressField fieldPrefix={registeredFormFields.event_address} disableCard />
-            <GpsPickerField name={registeredFormFields.event_location} label={t('form:label.eventLocation')} />
-          </FormContent>
-        </HiddenCard>
-        <LocalesTabs
-          locales={locales}
-          locale={locale}
-          onLocaleChange={onLocaleChange}
-          render={(loc) => (
-            <>
-              <InputField
-                name={`${registeredFormFields.locale}.${loc}.${registeredFormFields.title}`}
-                label={t('form:label.title')}
-                isRequired
-              />
-              <TextareaField
-                name={`${registeredFormFields.locale}.${loc}.${registeredFormFields.description}`}
-                label={t('form:label.description')}
-              />
-              <WysiwygField
-                name={`${registeredFormFields.locale}.${loc}.${registeredFormFields.content}`}
-                label={t('form:label.content')}
-                isRequired
-              />
-            </>
-          )}
-        />
-        <CategoriesPickerField
-          name={registeredFormFields.categories}
-          label={t('form:label.categories')}
-          multiple
-          defaultValue={[]}
-        />
-        <TagsPickerField name={registeredFormFields.tags} label={t('form:label.tags')} multiple defaultValue={[]} />
-        <AttachmentsPickerField
-          name={registeredFormFields.attachments}
-          label={t('form:label.attachments')}
-          multiple
-          defaultValue={[]}
-        />
+              </>
+            )}
+          />
+          <CategoriesPickerField
+            name={registeredFormFields.categories}
+            label={t('form:label.categories')}
+            multiple
+            defaultValue={[]}
+          />
+          <TagsPickerField name={registeredFormFields.tags} label={t('form:label.tags')} multiple defaultValue={[]} />
+          <AttachmentsPickerField
+            name={registeredFormFields.attachments}
+            label={t('form:label.attachments')}
+            multiple
+            defaultValue={[]}
+          />
+        </FormLayout>
+      </ControlledForm>
 
-        <DebugFormModel name="ArticlesDetailForm" />
-      </FormLayout>
-    </ControlledForm>
+      {/* We must render out of the main form due to context conflict */}
+      <DynamicPortal targetId={dynamicSlotId}>
+        <CommentsManager
+          isEnabled={isComments}
+          contentType={modelKeys.articles}
+          contentId={detailId !== newItemKey ? Number(detailId) : 0}
+        />
+      </DynamicPortal>
+    </>
   );
 };
 
